96d79f471ebe33a158e6cd646f6f13d406e60c32
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 12:17:56 -0700

    Update hp_4.py
    Done

--- src/hp_4.py
+++ src/hp_4.py
@@ -1,52 +1,71 @@
 # hp_4.py
-
 from datetime import datetime, timedelta
 from csv import DictReader, DictWriter
 from collections import defaultdict
 
+
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    reformatted_dates = []
+    for date_str in old_dates:
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        reformatted_date_str = date_obj.strftime('%d %b %Y')
+        reformatted_dates.append(reformatted_date_str)
+    return reformatted_dates
+
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    if not isinstance(start, str):
+        raise TypeError("start must be a string")
+    if not isinstance(n, int):
+        raise TypeError("n must be an integer")
+
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    date_sequence = [start_date + timedelta(days=i) for i in range(n)]
+    return date_sequence
+
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    if not isinstance(values, list) or not all(isinstance(v, (int, float)) for v in values):
+        raise TypeError("values must be a list of numerical values")
+
+    if not isinstance(start_date, str):
+        raise TypeError("start_date must be a string")
+
+    start_date_obj = datetime.strptime(start_date, '%Y-%m-%d')
+    date_sequence = [start_date_obj + timedelta(days=i) for i in range(len(values))]
+    result = [(date, value) for date, value in zip(date_sequence, values)]
+    return result
+
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    def calculate_late_fee(date_returned, date_due):
+        if date_returned > date_due:
+            days_late = (date_returned - date_due).days
+            return days_late * 0.25
+        return 0.0
 
-# The following main selection block will only run when you choose
-# "Run -> Module" in IDLE.  Use this section to run test code.  The
-# template code below tests the fees_report function.
-#
-# Use the get_data_file_path function to get the full path of any file
-# under the data directory.
+    late_fees = defaultdict(float)
+    with open(infile, 'r') as csv_file:
+        reader = DictReader(csv_file)
+        for row in reader:
+            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%y')
+            date_due = datetime.strptime(row['date_due'], '%m/%d/%y')
+            late_fee = calculate_late_fee(date_returned, date_due)
+            late_fees[row['patron_id']] += late_fee
 
-if __name__ == '__main__':
-    
-    try:
-        from src.util import get_data_file_path
-    except ImportError:
-        from util import get_data_file_path
-
-    # BOOK_RETURNS_PATH = get_data_file_path('book_returns.csv')
-    BOOK_RETURNS_PATH = get_data_file_path('book_returns_short.csv')
-
-    OUTFILE = 'book_fees.csv'
-
-    fees_report(BOOK_RETURNS_PATH, OUTFILE)
-
-    # Print the data written to the outfile
-    with open(OUTFILE) as f:
-        print(f.read())
+    with open(outfile, 'w', newline='') as out_file:
+        fieldnames = ['patron_id', 'late_fees']
+        writer = DictWriter(out_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for patron_id, late_fee in late_fees.items():
+            writer.writerow({'patron_id': patron_id, 'late_fees': round(late_fee, 2)})
