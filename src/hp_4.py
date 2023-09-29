d780949e0e635fe9499ce3830acbfc6fd339a994
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 12:03:19 -0700

    Update hp_4.py
    Done

--- src/hp_4.py
+++ src/hp_4.py
@@ -1,17 +1,22 @@
 # hp_4.py
-from datetime import datetime
+from datetime import datetime, timedelta
 from csv import DictReader, DictWriter
 from collections import defaultdict
 
-def reformat_dates(dates):
+def reformat_dates(old_dates):
+    """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
+    element to a format dd mmm yyyy--01 Jan 2001."""
     formatted_dates = []
-    for date_str in dates:
+    for date_str in old_dates:
         date_obj = datetime.strptime(date_str, '%Y-%m-%d')
         formatted_date_str = date_obj.strftime('%d %b %Y')
         formatted_dates.append(formatted_date_str)
     return formatted_dates
 
 def date_range(start, n):
+    """For input date string `start`, with format 'yyyy-mm-dd', returns
+    a list of of `n` datetime objects starting at `start` where each
+    element in the list is one day after the previous."""
     if not isinstance(start, str):
         raise TypeError("start must be a string")
     if not isinstance(n, int):
@@ -21,20 +26,23 @@ def date_range(start, n):
     return date_objects
 
 def add_date_range(values, start_date):
+    """Adds a daily date range to the list `values` beginning with
+    `start_date`.  The date, value pairs are returned as tuples
+    in the returned list."""
     date_objects = date_range(start_date, len(values))
     result = [(date_obj, value) for date_obj, value in zip(date_objects, values)]
     return result
 
 def fees_report(infile, outfile):
+    """Calculates late fees per patron id and writes a summary report to
+    outfile."""
     late_fees = defaultdict(float)
 
     with open(infile, 'r') as csv_file:
         reader = DictReader(csv_file)
         for row in reader:
-            date_checkout = datetime.strptime(row['date_checkout'], '%m/%d/%Y')
             date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
-            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%Y')  # Updated date format
-
+            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%y')
             if date_returned > date_due:
                 days_late = (date_returned - date_due).days
                 late_fee = days_late * 0.25
@@ -62,4 +70,3 @@ if __name__ == '__main__':
     # Print the data written to the outfile
     with open(OUTFILE) as f:
         print(f.read())
-
