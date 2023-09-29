f10539fd2e0dd2160447b528ab528c3699f7ea33
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 11:53:10 -0700

    Update hp_4.py
    Done

--- src/hp_4.py
+++ src/hp_4.py
@@ -3,11 +3,11 @@ from datetime import datetime, timedelta
 from csv import DictReader, DictWriter
 from collections import defaultdict
 
-def reformat_dates(old_dates):
+def reformat_dates(dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
     formatted_dates = []
-    for date_str in old_dates:
+    for date_str in dates:
         date_obj = datetime.strptime(date_str, '%Y-%m-%d')
         formatted_date_str = date_obj.strftime('%d %b %Y')
         formatted_dates.append(formatted_date_str)
@@ -15,7 +15,7 @@ def reformat_dates(old_dates):
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
-    a list of of `n` datetime objects starting at `start` where each
+    a list of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
     if not isinstance(start, str):
         raise TypeError("start must be a string")
@@ -41,8 +41,10 @@ def fees_report(infile, outfile):
     with open(infile, 'r') as csv_file:
         reader = DictReader(csv_file)
         for row in reader:
+            date_checkout = datetime.strptime(row['date_checkout'], '%m/%d/%Y')
             date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
             date_returned = datetime.strptime(row['date_returned'], '%m/%d/%y')
+            
             if date_returned > date_due:
                 days_late = (date_returned - date_due).days
                 late_fee = days_late * 0.25
