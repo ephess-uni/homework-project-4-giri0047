9c6ae43d0cf8873929c688e34989fa25aae66787
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 12:06:23 -0700

    Update hp_4.py
    Done

--- src/hp_4.py
+++ src/hp_4.py
@@ -6,31 +6,30 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    formatted_dates = []
+    new_dates = []
     for date_str in old_dates:
         date_obj = datetime.strptime(date_str, '%Y-%m-%d')
-        formatted_date_str = date_obj.strftime('%d %b %Y')
-        formatted_dates.append(formatted_date_str)
-    return formatted_dates
+        new_date_str = date_obj.strftime('%d %b %Y')
+        new_dates.append(new_date_str)
+    return new_dates
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    if not isinstance(start, str):
-        raise TypeError("start must be a string")
-    if not isinstance(n, int):
-        raise TypeError("n must be an integer")
+    if not isinstance(start, str) or not isinstance(n, int):
+        raise TypeError("start must be a string, and n must be an integer")
+
     start_date = datetime.strptime(start, '%Y-%m-%d')
-    date_objects = [start_date + timedelta(days=i) for i in range(n)]
-    return date_objects
+    date_list = [start_date + timedelta(days=i) for i in range(n)]
+    return date_list
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    date_objects = date_range(start_date, len(values))
-    result = [(date_obj, value) for date_obj, value in zip(date_objects, values)]
+    date_list = date_range(start_date, len(values))
+    result = [(date, value) for date, value in zip(date_list, values)]
     return result
 
 def fees_report(infile, outfile):
@@ -39,8 +38,8 @@ def fees_report(infile, outfile):
     late_fees = defaultdict(float)
 
     with open(infile, 'r') as csv_file:
-        reader = DictReader(csv_file)
-        for row in reader:
+        csv_reader = DictReader(csv_file)
+        for row in csv_reader:
             date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
             date_returned = datetime.strptime(row['date_returned'], '%m/%d/%y')
             if date_returned > date_due:
@@ -48,12 +47,12 @@ def fees_report(infile, outfile):
                 late_fee = days_late * 0.25
                 late_fees[row['patron_id']] += late_fee
 
-    with open(outfile, 'w', newline='') as csv_file:
+    with open(outfile, 'w', newline='') as csv_output:
         fieldnames = ['patron_id', 'late_fees']
-        writer = DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
+        csv_writer = DictWriter(csv_output, fieldnames=fieldnames)
+        csv_writer.writeheader()
         for patron_id, fee in late_fees.items():
-            writer.writerow({'patron_id': patron_id, 'late_fees': '{:.2f}'.format(fee)})
+            csv_writer.writerow({'patron_id': patron_id, 'late_fees': "{:.2f}".format(fee)})
 
 if __name__ == '__main__':
     try:
@@ -61,12 +60,11 @@ if __name__ == '__main__':
     except ImportError:
         from util import get_data_file_path
 
-    # Use the full dataset 'book_returns.csv'
-    BOOK_RETURNS_PATH = get_data_file_path('book_returns.csv')
+    BOOK_RETURNS_PATH = get_data_file_path('book_returns_short.csv')
     OUTFILE = 'book_fees.csv'
-
     fees_report(BOOK_RETURNS_PATH, OUTFILE)
 
     # Print the data written to the outfile
     with open(OUTFILE) as f:
         print(f.read())
+
