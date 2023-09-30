2fd8e275330bd745843f01c4a0d67375a68d450c
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 21:57:11 -0700

    Update hp_4.py
    Complete

--- src/hp_4.py
+++ src/hp_4.py
@@ -1,4 +1,6 @@
-# hp_4.py
+
+# hp_4.py#
+
 from datetime import datetime, timedelta
 from csv import DictReader, DictWriter
 from collections import defaultdict
@@ -7,64 +9,94 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    reformatted_dates = []
-    for date_str in old_dates:
-        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
-        reformatted_date_str = date_obj.strftime('%d %b %Y')
-        reformatted_dates.append(reformatted_date_str)
-    return reformatted_dates
-
+    modified_date_list=[]
+    for dat in old_dates:
+        modified_date_list.append(datetime.strptime(dat, "%Y-%m-%d").strftime("%d %b %Y"))
+    return modified_date_list
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
+    
     if not isinstance(start, str):
-        raise TypeError("start must be a string")
-    if not isinstance(n, int):
-        raise TypeError("n must be an integer")
-
-    start_date = datetime.strptime(start, '%Y-%m-%d')
-    date_sequence = [start_date + timedelta(days=i) for i in range(n)]
-    return date_sequence
+        raise TypeError
+    elif not isinstance(n, int):
+        raise TypeError
+    else:
+        added_list=[]
+        for inc in range(0,n):
+            added_list.append(datetime.strptime(start,"%Y-%m-%d")  + timedelta(days=inc))
+        return added_list
 
 
 def add_date_range(values, start_date):
-    """Adds a daily date range to the list `values` beginning with
+    """Adds a daily date range to the list `values` beginning with 
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    if not isinstance(values, list) or not all(isinstance(v, (int, float)) for v in values):
-        raise TypeError("values must be a list of numerical values")
-
-    if not isinstance(start_date, str):
-        raise TypeError("start_date must be a string")
-
-    start_date_obj = datetime.strptime(start_date, '%Y-%m-%d')
-    date_sequence = [start_date_obj + timedelta(days=i) for i in range(len(values))]
-    result = [(date, value) for date, value in zip(date_sequence, values)]
-    return result
+    added_list=[]
+    for i, elem in enumerate(values):
+        dat_list=[]       
+        dat_list.append(datetime.strptime(start_date,"%Y-%m-%d")  + timedelta(days=i))
+        dat_list.append(elem)
+        added_list.append(tuple(dat_list))
+    return added_list
 
 
 def fees_report(infile, outfile):
-    with open(infile, mode='r') as file:
-        reader = DictReader(file)
-        fees = defaultdict(float)
+    """Calculates late fees per patron id and writes a summary report to
+    outfile."""
+  
+    with open(infile) as file:
+        added_list=[]
+        read_csv_obj = DictReader(file)
+        for record in read_csv_obj:
+            temp_dict={}
+            late_fee_days=datetime.strptime(record['date_returned'],'%m/%d/%Y')- datetime.strptime(record['date_due'],'%m/%d/%Y') 
+            if(late_fee_days.days>0):
+                temp_dict["patron_id"]=record['patron_id']
+                temp_dict["late_fees"]=round(late_fee_days.days*0.25, 2)
+                added_list.append(temp_dict)
+            else:
+                temp_dict["patron_id"]=record['patron_id']
+                temp_dict["late_fees"]=float(0)
+                added_list.append(temp_dict)
+                
+        temp_dict_2 = {}
+        for dict in added_list:
+            key = (dict['patron_id'])
+            temp_dict_2[key] = temp_dict_2.get(key, 0) + dict['late_fees']
+        updated_list = [{'patron_id': key, 'late_fees': value} for key, value in temp_dict_2.items()]
+        
+        for dict in updated_list:
+            for key,value in dict.items():
+                if key == "late_fees":
+                    if len(str(value).split('.')[-1]) != 2:
+                        dict[key] = str(value)+"0"
 
-        for row in reader:
-            # Parse date strings with the correct format
-            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%Y')
-            date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
 
-            if date_returned > date_due:
-                days_late = (date_returned - date_due).days
-                late_fee = days_late * 0.25
-                patron_id = row['patron_id']
-                fees[patron_id] += late_fee
-
-    with open(outfile, mode='w', newline='') as file:
-        writer = DictWriter(file, fieldnames=['patron_id', 'late_fees'])
+   
+    with open(outfile,"w", newline="") as file:
+        col = ['patron_id', 'late_fees']
+        writer = DictWriter(file, fieldnames=col)
         writer.writeheader()
-        for patron_id, late_fee in fees.items():
-            # Format late_fee with 2 decimal places
-            late_fee_str = f'{late_fee:.2f}'
-            writer.writerow({'patron_id': patron_id, 'late_fees': late_fee_str})
+        writer.writerows(updated_list)
+
+
+
+
+if __name__ == '__main__':
+    
+    try:
+        from src.util import get_data_file_path
+    except ImportError:
+        from util import get_data_file_path
+
+    BOOK_RETURNS_PATH = get_data_file_path('book_returns_short.csv')
+
+    OUTFILE = 'book_fees.csv'
+
+    fees_report(BOOK_RETURNS_PATH, OUTFILE)
+
+    with open(OUTFILE) as f:
+        print(f.read())
